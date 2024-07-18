c2c1468c601c1324dab6d9e73d4cca31cff54b0b
IbrahimElsharawy <ibrahimelsha33rawy@gmail.com>
2024-07-18 16:40:26 +0300

    Add Background photo in component blog

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import bgBlog from '../../assets/blog.jpg'
 
 const Blog = () => {
+  const bgStyle = {
+    backgroundImage: `url(${bgBlog})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
   return (
-    <div>Blog</div>
+    <div>
+      <div className="w-full h-[250px] relative text-white" style={bgStyle}>
+        <div
+          className="w-full h-full relative z-10"
+          style={{ backgroundColor: "rgba(0,0,0,.6)" }}
+        >
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full text-center">
+            <h2 className="text-2xl md:text-4xl font-bold uppercase">blog</h2>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
